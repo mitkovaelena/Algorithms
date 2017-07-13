3102669a935d1b15aa0b2d3fd385089c9adfdd63
mitkovaelena <mitkova_elena@yahoo.com>
2017-07-13 15:38:49 +0300

    Needles - solved

---
+++ 02.SortingAndSearching/src/exercises/needles/NeedlesBinarySearch.java
@@ -0,0 +1,88 @@
+package exercises.needles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NeedlesBinarySearch {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
+        String[] line = reader.readLine().split("\\s+");
+        int[] sequence = new int[line.length];
+
+        for (int i = 0; i < line.length; i++) {
+            sequence[i] = Integer.parseInt(line[i]);
+        }
+
+        line = reader.readLine().split("\\s+");
+        int[] nums = new int[line.length];
+
+        for (int i = 0; i < line.length; i++) {
+            nums[i] = Integer.parseInt(line[i]);
+        }
+
+        findIndexes(sequence, nums);
+    }
+
+    private static void findIndexes(int[] sequence, int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            sb.append(binarySearch(sequence, num, 0, sequence.length - 1)).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    private static int binarySearch(int[] sequence, int key, int lo, int hi) {
+
+        if (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            while (mid > 0 && sequence[mid] == 0) {
+                mid--;
+            }
+
+            if (mid == 0 && (sequence[mid] == 0 || sequence[mid] >= key)) {
+                return mid;
+            }
+            if (mid == sequence.length - 1 && sequence[mid] < key) {
+                return mid + 1;
+            }
+
+            if (key < sequence[mid]) {
+                if (mid >= 1 && sequence[mid - 1] != 0 && sequence[mid - 1] < key) {
+                    return mid;
+                }
+                return binarySearch(sequence, key, lo, mid);
+
+            } else if (key > sequence[mid]) {
+                int temp = mid;
+                while (mid + 2 < sequence.length && sequence[mid + 1] == 0) {
+                    mid++;
+                }
+                if (sequence[mid + 1] >= key) {
+                    return temp + 1;
+                }
+                return binarySearch(sequence, key, mid + 1, hi);
+
+            } else {
+                while (mid >= 0 && (sequence[mid] == key || sequence[mid] == 0)) {
+                    mid--;
+                }
+                return mid + 1;
+            }
+        }
+
+        if (lo == hi && sequence[lo] == key) {
+            while (lo >= 0 && (sequence[lo] == key || sequence[lo] == 0)) {
+                lo--;
+            }
+            return lo + 1;
+        }
+        if (lo == hi && sequence[lo] > key) {
+            return lo;
+        }
+        return lo + 1;
+    }
+}
---
+++ 02.SortingAndSearching/src/exercises/needles/NeedlesIterative.java
@@ -0,0 +1,59 @@
+package exercises.needles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NeedlesIterative {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        reader.readLine();
+        String[] inputLine = reader.readLine().split(" ");
+
+        int sequenceLen = inputLine.length;
+        int[] sequence = new int[sequenceLen];
+
+        for (int i = 0; i < sequenceLen; i++) {
+            sequence[i] = Integer.parseInt(inputLine[i]);
+        }
+
+        inputLine = reader.readLine().split("\\s+");
+        int[] nums = new int[inputLine.length];
+
+        for (int i = 0; i < inputLine.length; i++) {
+            nums[i] = Integer.parseInt(inputLine[i]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            int mid = sequenceLen / 2;
+            int start = 0;
+            int end = sequenceLen;
+            while (mid > 0 && (sequence[mid] == 0 || sequence[mid] == num)) {
+                mid--;
+            }
+            if (sequence[mid] <= num) start = mid;
+            else end = mid + 1;
+            sb.append(findIndex(sequence, num, start, end)).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    private static int findIndex(int[] sequence, int num, int start, int end) {
+        for (int i = start; i < end; i++) {
+            int temp = i;
+            while (temp + 1 < end && sequence[temp] == 0) {
+                temp++;
+            }
+            if (sequence[temp] == 0) {
+                return i;
+            }
+            if (sequence[temp] >= num) {
+                return i;
+            }
+        }
+        return end;
+    }
+}
